89a32cb1f7639b75701f3805c15133910eefd0fe
Rahulbalakrishnan <newtoncorpse@gmail.com>
2023-09-14 20:46:36 +0530

    try2!

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,30 @@
-// Resource: https://clerk.com/docs/nextjs/middleware#auth-middleware
-// Copy the middleware code as it is from the above resource
+// // Resource: https://clerk.com/docs/nextjs/middleware#auth-middleware
+// // Copy the middleware code as it is from the above resource
+
+// import { authMiddleware } from "@clerk/nextjs";
+
+
+
+// export default authMiddleware({
+//   // An array of public routes that don't require authentication.
+//   publicRoutes: ["/api/webhook/clerk"],
+
+//   // An array of routes to be ignored by the authentication middleware.
+//   ignoredRoutes: ["/api/webhook/clerk"],
+// });
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+
 
 import { authMiddleware } from "@clerk/nextjs";
-
 export default authMiddleware({
-  // An array of public routes that don't require authentication.
-  publicRoutes: ["/api/webhook/clerk"],
-
-  // An array of routes to be ignored by the authentication middleware.
-  ignoredRoutes: ["/api/webhook/clerk"],
+  // "/" will be accessible to all users
+  publicRoutes: ["/"]
 });
-
+ 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+      matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
