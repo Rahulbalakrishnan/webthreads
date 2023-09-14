d18b8a578c89e8edfdae3531820057f440f740ea
Rahulbalakrishnan <newtoncorpse@gmail.com>
2023-09-14 20:36:14 +0530

    try1!

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,7 @@ export default authMiddleware({
   publicRoutes: ["/api/webhook/clerk"],
 
   // An array of routes to be ignored by the authentication middleware.
-  ignoredRoutes: ["/"],
+  ignoredRoutes: ["/api/webhook/clerk"],
 });
 
 export const config = {
